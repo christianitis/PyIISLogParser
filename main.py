131647c2fc1b64e61ec8632a8aa68b36ba8fa92a
Christian Hollinger <chollinger@DesignChoice01.local>
2021-11-24 15:50:17 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import os.path
+from datetime import *
+import sqlite3
+import matplotlib
+import exchangelib
+import pandas
+
+import summary
+LOGS_FROM_DAYS = -3  # Go back X amount of days
+LOGDIRECTORY = "//DCVM-WEB/c$/inetpub/logs/LogFiles/W3SVC1/"
+DTSTRING = datetime.now().strftime("%y%m%d")
+
+connection = sqlite3.connect(os.path.realpath(r"\Users\chollinger\iisparser") + "\\" + DTSTRING + str(datetime.now().second) + ".db")  # TODO holy crap lol this sucks
+connection.execute("CREATE TABLE logtable (dt TEXT, uri TEXT, status INTEGER, timereq INTEGER);")
+
+currentdate = datetime.now()
+filenames = []
+
+# Create filenames for the log files to be loaded
+i = 0
+while i > LOGS_FROM_DAYS:
+    dt = datetime.now() + timedelta(days=i)
+    filenames.append(os.path.join(LOGDIRECTORY, ("u_ex%s_x.log" % dt.strftime("%y%m%d"))))
+    i -= 1
+del i
+
+filenames.reverse()  # Reverse so the oldest logs are displayed last
+
+cursor = connection.cursor()
+
+for filename in filenames:
+    try:
+        print("Opening " + filename + "...")
+        file = open(filename)
+        lines = file.readlines()
+
+        for line in lines:
+            if line.startswith('#'):
+                continue
+
+            values = line.split(' ')
+            sqlcommand = ("""INSERT INTO logtable VALUES("%s", "%s", %s, %s);""" %
+                          (values[0] + " " + values[1], values[4], values[11], values[14])).replace('\n', '')
+            #print(sqlcommand)
+            cursor.execute(sqlcommand)
+            summary.total_requests += 1
+
+            if int(values[11]) >= 400:
+                summary.total_errors += 1
+
+    except IOError:
+        print("Could not load log file \"" + filename + "\", skipping.")
+    except:
+        print("Could not execute SQL command. Skipping...")
+
+summary.initialize(connection.cursor(), LOGS_FROM_DAYS)
+connection.commit()
+
+connection.close()
---
+++ summary.py
@@ -0,0 +1,152 @@
+import datetime
+import datetime as dt
+import exchangelib
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import numpy as np
+from exchangelib import attachments
+
+# TODO: All of this needs a major cleanup, it looks like total crap
+
+total_requests = 0
+total_errors = 0
+
+requests_by_time = {}
+errors_by_time = {}
+
+
+def increment_rbt(datet: datetime.datetime, iserror: bool, ):
+    # To round it to the hour for better display on the chart/smaller memory footprint
+    hour_dt = dt.datetime.strptime(datet.strftime("%m/%d/%y %H:00:00"), "%m/%d/%y %H:%M:%S")
+
+    if iserror:
+        if not errors_by_time.__contains__(hour_dt):
+            errors_by_time[hour_dt] = 1
+        else:
+            errors_by_time[hour_dt] += 1
+
+    else:
+        if not requests_by_time.__contains__(hour_dt):
+            requests_by_time[hour_dt] = 1
+        else:
+            requests_by_time[hour_dt] += 1
+
+
+def initialize(cursor, logdays):
+    print("Done loading logs, creating charts...")
+    cursor.execute("SELECT dt, status FROM logtable;")
+
+    for row in cursor.fetchall():
+        datet = dt.datetime.fromisoformat(row[0])
+        if row[1] >= 500:
+            # print("inc error")
+            increment_rbt(datet, True)
+        else:
+            # print("inc normal")
+            increment_rbt(datet, False)
+
+    plt.title("Requests/Errors Per Hour")
+    plt.gca().xaxis_date()
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+    plt.gca().xaxis.set_minor_locator(mdates.HourLocator(byhour=12))
+    plt.plot(requests_by_time.keys(), requests_by_time.values())
+    plt.plot(errors_by_time.keys(), errors_by_time.values())
+    plt.gcf().autofmt_xdate()
+    plt.savefig("requestsanderrors.svg")
+    plt.close()
+
+    cursor.execute("SELECT uri, COUNT(*) AS c FROM logtable GROUP BY uri HAVING c > 1 ORDER BY c DESC LIMIT 10;")
+
+    most_requested_pages = {}
+    for row in cursor.fetchall():
+        most_requested_pages[row[0]] = row[1]
+
+    fig, ax = plt.subplots()
+    ax.pie(most_requested_pages.values(), labels=most_requested_pages.keys(), textprops={'fontsize': 8})
+    ax.axis('equal')
+    plt.title("10 Most Requested Pages")
+    plt.tight_layout()
+    plt.savefig("mostviewedpages.svg")
+    plt.close()
+
+    cursor.execute(
+        "SELECT uri, COUNT(*) FROM logtable WHERE status >= 400 GROUP BY uri ORDER BY COUNT(*) DESC LIMIT 20;")
+    errors_by_page = {}
+    for row in cursor.fetchall():
+        errors_by_page[row[0]] = int(row[1])
+
+    y_pos = np.arange(len(errors_by_page.keys()))
+    plt.barh(y_pos, errors_by_page.values(), color=(1, 0, 0, 1))
+    plt.yticks(y_pos, errors_by_page.keys(), fontsize='x-small')
+    plt.title("Top 20 Most Erroneous Pages")
+    plt.tight_layout()
+    plt.savefig("mosterroneouspages.svg")
+    plt.close()
+
+    cursor.execute("SELECT AVG(timereq) FROM logtable;")
+    avg_time_req = cursor.fetchone()[0]
+
+    credentials = exchangelib.Credentials("autoreports", "slimyFishbowls2")
+    config = exchangelib.Configuration(credentials=credentials,
+                                       service_endpoint="https://mail.dccabinetry.com/ews/exchange.asmx")
+    account = exchangelib.Account(
+        primary_smtp_address="amailer@dccabinetry.com", config=config, credentials=credentials)
+
+    message = exchangelib.Message()
+
+    # TODO clean this up dude lmao
+    chart1 = attachments.FileAttachment(name="mosterroneouspages.svg",
+                                        content=open("mosterroneouspages.svg", 'rb').read(),
+                                        content_id="mosterroneouspages.svg")
+
+    chart2 = attachments.FileAttachment(name="requestsanderrors.svg",
+                                        content=open("requestsanderrors.svg", 'rb').read(),
+                                        content_id="requestsanderrors.svg")
+
+    chart3 = attachments.FileAttachment(name="mostviewedpages.svg",
+                                        content=open("mostviewedpages.svg", 'rb').read(),
+                                        content_id="mostviewedpages.svg")
+
+    message.attach([chart1, chart2, chart3])
+    from_to = (datetime.date.today() + datetime.timedelta(days=logdays), datetime.date.today())
+    # The HTML text for the message.
+    txt = "<html>\n" \
+          "<body>\n" \
+          "From " + from_to[0].isoformat() + " to " + from_to[1].isoformat() + " the server processed " + \
+          str(total_requests) + " requests, " + str(total_errors) + \
+          " (%" + str(((total_errors / total_requests) * 100.0).__round__(2)) + ") of which were errors.<br>" \
+          "On average, it took about " + str(avg_time_req.__round__(3)) + " milliseconds to respond to requests.<br>" \
+          "<img src=\"cid:requestsanderrors.svg\"><br>" \
+          "The top 20 most requested pages were:" \
+          "<ol>"
+
+    for page in most_requested_pages.items():
+        txt += "<li>" + page[0] + " (" + str(page[1]) + " requests)</li>"
+
+    txt += "</ol>" \
+           "<img src=\"cid:mostviewedpages.svg\"><br>" \
+           "The top 20 pages which returned the most errors were:" \
+           "<ol>"
+    for page in errors_by_page.items():
+        txt += "<li>" + page[0] + " (" + str(page[1]) + " errors)</li>"
+
+    txt += "</ol>" \
+           "<img src=\"cid:mosterroneouspages.svg\"><br>" \
+           "<em> If you have any further questions, comments, suggestions, or would like access to the full" \
+           " database file; send an email to chollinger@dccabinetry.com.</em><br>" \
+           "<strong>Don't reply to this email, because you won't get a response.</strong>" \
+           "</body></html>"
+
+    message.subject = "Automated Server Report For %s to %s (Do Not Reply)" % \
+                      (from_to[0].isoformat(), from_to[1].isoformat())
+    message.body = exchangelib.HTMLBody(txt)
+
+    message.account = account
+    message.to_recipients = [
+        exchangelib.Mailbox(email_address="chollinger@dccabinetry.com"),
+        exchangelib.Mailbox(email_address="dcrain@dccabinetry.com"),
+        exchangelib.Mailbox(email_address="cbancroft@dccabinetry.com")
+        ]
+
+    #message.send()
